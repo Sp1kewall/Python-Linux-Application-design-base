32a53116f18f6ce11af840d5109da8bd5d5bc91b
The Conqueror of KAMAZ <105853687+SpaceCoreOfficial@users.noreply.github.com>
2023-11-09 19:07:49 +0500

    Update
    yes

---
+++ Executable/app.py
@@ -0,0 +1,22 @@
+from yamlread import *
+from update import *
+import os
+
+
+sourcemode = 0
+
+if os.path.isfile("source"):
+    sourcemode = 1
+else:
+    pass
+
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
